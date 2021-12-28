ac2d8b0a8b09c4df3013a9664b75ad5a3b272063
Vinicius <viniciusflexa@gmail.com>
2021-12-28 16:48:16 -0300

    Adicionando anotações

---
+++ Projeto/objeto/objetoRevisao.js
@@ -0,0 +1,42 @@
+// coleção dinâmica de pares chave/valor
+const produto = new Object
+produto.nome = 'Cadeira'
+produto['marca do produto'] = 'Generica'
+produto.preco = 220
+
+console.log(produto)
+delete produto.preco
+delete produto['marca do produto']
+console.log(produto)
+
+const carro = {
+    modelo: 'A4',
+    valor: 89000,
+    proprietario:{
+        nome: 'Vinicius',
+        idade: 20,
+        endereco:{
+            logradouro: 'Rua ABC',
+            numero: 123
+        }
+    },
+    condutores: [{
+        nome: 'Junior',
+        idade: 19
+    },{
+        nome: 'Ana',
+        idade: 42
+    }],
+    calculaValorSeguro: function(){
+        // ...
+    }
+}
+
+carro.proprietario.endereco.numero = 1000
+carro['proprietario']['endereco']['logradouro'] = 'Av Gigante'
+console.log(carro)
+
+delete carro.condutores
+delete carro.proprietario.endereco
+delete carro.calculaValorSeguro
+console.log(carro)
