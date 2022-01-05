aff790aa75309e96336e1f4b3cddcb717a755073
Vinicius <viniciusflexa@gmail.com>
2022-01-05 15:20:40 -0300

    Finalizando o conteúdo de Arrays

---
+++ JavaScript/Projeto/array/flatMap.js
@@ -0,0 +1,32 @@
+const escola = [{
+    nome: 'Turma M1',
+    alunos: [{
+        nome: 'Gustavo',
+        nota: 8.1
+    }, {
+        nome: 'Ana',
+        nota: 9.3
+    }]
+}, {
+    nome: 'Turma M2',
+    alunos: [{
+        nome: 'Rebeca',
+        nota: 8.9
+    }, {
+        nome: 'Roberto',
+        nota: 7.3
+    }]
+}]
+
+const getNotaAluno = a => a.nota
+const getNotasTurma = t => t.alunos.map(getNotaAluno)
+
+const notas1 = escola.map(getNotasTurma)
+console.log(notas1)
+
+Array.prototype.flatMap = function(callback){
+    return Array.prototype.concat.apply([], this.map(callback))
+}
+
+const notas2 = escola.flatMap(getNotasTurma)
+console.log(notas2)
