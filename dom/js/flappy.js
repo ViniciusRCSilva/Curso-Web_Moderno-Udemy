a78b77509ffe04423bce7bb56bc70b8c2ea2633b
Vinicius <viniciusflexa@gmail.com>
2022-02-07 11:29:14 -0300

    Finalizando Desafio FlappyBird

--- dom/js/flappy.js
+++ dom/js/flappy.js
@@ -70,9 +70,97 @@ function Barreiras(altura, largura, abertura, espaco, notificarPonto){
     }
 }
 
-const barreiras = new Barreiras(700, 1200, 200, 400)
-const areaDeJogo = document.querySelector('[wm-flappy]')
-barreiras.pares.forEach(par => areaDeJogo.appendChild(par.elemento))
-setInterval(() =>{
-    barreiras.animar()
-}, 20)
+function Passaro(alturaJogo){
+    let voando = false
+
+    this.elemento = novoElemento('img', 'passaro')
+    this.elemento.src = 'imgs/passaro.png'
+
+    this.getY = () => parseInt(this.elemento.style.bottom.split('px')[0])
+    this.setY = y => this.elemento.style.bottom = `${y}px`
+
+
+    window.onkeydown = e => voando = true
+/*  window.onkeydown   = e => {
+        voando = true
+        setTimeout(() => { voando = false }, 300) 
+    } */
+    window.onkeyup = e => voando = false
+
+    this.animar = () =>{
+        const novoY = this.getY() + (voando ? 6 : -5)
+        const alturaMaxima = alturaJogo - this.elemento.clientHeight
+
+        if(novoY <= 0){
+            this.setY(0)
+        }else if(novoY >= alturaMaxima){
+            this.setY(alturaMaxima)
+        }else{
+            this.setY(novoY)
+        }
+    }
+
+    this.setY(alturaJogo / 2)
+}
+
+
+function Progresso(){
+    this.elemento = novoElemento('span', 'progresso')
+    this.atualizarPontos = pontos => {
+        this.elemento.innerHTML = pontos
+    }
+    this.atualizarPontos(0)
+}
+
+function estaSobrepostos(elementoA, elementoB){
+    const a = elementoA.getBoundingClientRect()
+    const b = elementoB.getBoundingClientRect()
+
+    const horizontal = a.left + a.width >= b.left && b.left + b.width >= a.left
+    const vertical = a.top + a.height >= b.top && b.top + b.height >= a.top
+
+    return horizontal && vertical
+}
+
+function colidiu(passaro, barreiras) {
+    let colidiu = false
+
+    barreiras.pares.forEach(ParDeBarreiras => {
+        if(!colidiu){
+            const superior = ParDeBarreiras.superior.elemento
+            const inferior = ParDeBarreiras.inferior.elemento
+            colidiu = estaSobrepostos(passaro.elemento, superior) || estaSobrepostos(passaro.elemento, inferior)
+        }
+    })
+    return colidiu
+}
+
+function FlappyBird(){
+    let pontos = 0
+
+    const areaDoJogo = document.querySelector('[wm-flappy]')
+    const altura = areaDoJogo.clientHeight
+    const largura = areaDoJogo.clientWidth
+
+    const progresso = new Progresso()
+    const barreiras = new Barreiras(altura, largura, 200, 400, () => progresso.atualizarPontos(++pontos))
+    const passaro = new Passaro(altura)
+
+    areaDoJogo.appendChild(progresso.elemento)
+    areaDoJogo.appendChild(passaro.elemento)
+    barreiras.pares.forEach(par => areaDoJogo.appendChild(par.elemento))
+
+    this.start = () => {
+        /* loop do jogo */
+        const temporizador = setInterval(() =>{
+            barreiras.animar()
+            passaro.animar()
+
+            if(colidiu(passaro, barreiras)){
+                clearInterval(temporizador)
+            }
+        }, 20)
+    }
+}
+
+new FlappyBird().start()
