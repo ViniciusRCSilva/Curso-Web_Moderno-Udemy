0f69909a5ebe46a3f2e3ac9e970a5978c8f2b86e
Vinicius <viniciusflexa@gmail.com>
2022-02-08 12:14:59 -0300

    Atualizando Conteúdo de Ajax

--- Ajax/server.js
+++ Ajax/server.js
@@ -6,5 +6,33 @@ app.use(express.static('.'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-app.get('/teste', (req, res) => res.send('Ok'))
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: function(req, file, callback){
+        callback(null, './upload')
+    },
+    filename: function(req, file, callback){
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({ storage }).single('arquivo')
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if(err){
+            return res.end('Ocorreu um erro')
+        }
+        res.end('Concluído com sucesso')
+    })
+})
+
+app.post('/formulario', (req, res) =>{
+    res.send({
+        ...req.body,
+        id: 1
+    })
+})
+
 app.listen(8080, () => console.log('Executando...'))
