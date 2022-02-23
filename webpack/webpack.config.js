84bfa682c83af54a50492962f5d0a9b3cb81a360
Vinicius <viniciusflexa@gmail.com>
2022-02-23 17:41:50 -0300

    Finalizando Conteúdo de Webpack

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,12 +1,28 @@
+const modoDev = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
+    mode: modoDev ? 'development': 'production',
     entry: './src/principal.js',
     output: {
         filename: 'principal.js',
         path: __dirname + '/public'
+    },
+    devServer: {
+        contentBase: "./public",
+        port: 9000
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
     },
     plugins: [
         new MiniCssExtractPlugin({
@@ -22,6 +38,9 @@ module.exports = {
                 'css-loader', /* interpreta @import, url()... */
                 'sass-loader'
             ]
+        }, {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: ['file-loader']  
         }]
     }
 }
