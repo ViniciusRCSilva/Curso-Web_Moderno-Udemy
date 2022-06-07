348e99eef6ab53d6cac93bb545e091c2bf8248bf
Vinicius <viniciusflexa@gmail.com>
2022-06-07 12:35:04 -0300

    conteúdo => componentes

---
+++ react/exercicios/src/components/BomDia.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function(props){
+    return (
+        <>
+            <h1>Bom dia {props.nome}</h1>
+            <h2>Até breve!</h2>
+        </>
+    )
+}
--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -1,6 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const elemento = <h1>React</h1> 
+// import Primeiro from './components/Primeiro'
+// import BomDia from './components/BomDia'
+// import { BoaTarde, BoaNoite } from './components/Multiplos'
+import Saudacao from './components/Saudacao'
 
-ReactDOM.render(elemento, document.getElementById('root'))
+/* const elemento = <h1>React</h1>  */
+
+ReactDOM.render(
+    <div>        
+        <Saudacao />
+    </div>
+, document.getElementById('root'))
