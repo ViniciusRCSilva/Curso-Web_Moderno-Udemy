9f36630915b9230dee8f8143cb1321faa7243252
Vinicius <viniciusflexa@gmail.com>
2022-06-07 22:14:06 -0300

    finalizando conceito de componentes em react

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -4,12 +4,18 @@ import ReactDOM from 'react-dom'
 // import Primeiro from './components/Primeiro'
 // import BomDia from './components/BomDia'
 // import { BoaTarde, BoaNoite } from './components/Multiplos'
-import Saudacao from './components/Saudacao'
+// import Saudacao from './components/Saudacao'
+import Pai from './components/Pai'
+import Filho from './components/Filho'
 
 /* const elemento = <h1>React</h1>  */
 
 ReactDOM.render(
     <div>        
-        <Saudacao />
+        <Pai nome="Paulo" sobrenome="Silva" >
+            <Filho nome="Pedro" sobrenome="Silva"/>
+            <Filho nome="Paulo" sobrenome="Silva"/>
+            <Filho nome="Carla" sobrenome="Silva"/>
+        </Pai>
     </div>
 , document.getElementById('root'))
