e7ad0d77b5e3bc5991c187bba77b4e82a1736ee5
Vinicius <viniciusflexa@gmail.com>
2022-06-11 14:33:14 -0300

    ajustando os routers da página

--- react/crud/frontend/src/components/template/Logo.jsx
+++ react/crud/frontend/src/components/template/Logo.jsx
@@ -1,8 +1,9 @@
 import './Logo.css'
 import logo from '../../assests/images/logo.png'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default props => 
     <aside className='logo'>
-        <a className='logo' href="/"><img src={logo} alt="logo" /></a>
+        <Link className='logo' to="/"><img src={logo} alt="logo" /></Link>
     </aside>
--- react/crud/frontend/src/main/App.jsx
+++ react/crud/frontend/src/main/App.jsx
@@ -5,13 +5,17 @@ import React from 'react'
 
 import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
-import Home from '../components/home/Home'
+import Routes from './Routes'
 import Footer from '../components/template/Footer'
 
+import { BrowserRouter } from 'react-router-dom'
+
 export default props =>
-    <div className="app">
-        <Logo />
-        <Nav />
-        <Home />
-        <Footer />
-    </div>
+    <BrowserRouter>
+        <div className="app">
+            <Logo />
+            <Nav />
+            <Routes />
+            <Footer />
+        </div>
+    </BrowserRouter>
