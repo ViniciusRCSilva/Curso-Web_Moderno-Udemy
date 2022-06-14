64966ddaf06546ab89459171c1d79a02b430691a
Vinicius <viniciusflexa@gmail.com>
2022-06-14 19:38:45 -0300

    componente layout

--- aulao_nextjs/fundamentos/src/pages/estiloso.jsx
+++ aulao_nextjs/fundamentos/src/pages/estiloso.jsx
@@ -1,11 +1,12 @@
 import styles from '../styles/Estiloso.module.css'
-import Link from 'next/link'
+import Layout from '../components/Layout'
 
 export default function Estiloso(){
     return(
-        <div className={styles.roxo}>
-            <Link href='/'>Voltar</Link>
-            <h1>Estilo usando Módulos CSS</h1>
-        </div>
+        <Layout titulo="Exemplo de CSS Modularizado">
+            <div className={styles.roxo}>
+                <h1>Estilo usando Módulos CSS</h1>
+            </div>
+        </Layout>
     )
 }
