b5b194f3103afb316027089d6eedb37895957b8f
Vinicius <viniciusflexa@gmail.com>
2022-06-14 11:31:19 -0300

    conceitos de css e módulo css

---
+++ aulao_nextjs/fundamentos/src/pages/estiloso.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Estiloso.module.css'
+
+export default function Estiloso(){
+    return(
+        <div className={styles.roxo}>
+            <h1>Estilo usando Módulos CSS</h1>
+        </div>
+    )
+}
