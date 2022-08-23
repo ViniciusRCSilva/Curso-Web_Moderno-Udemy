266210294694265e10406b8655f904ab3bab34c7
Vinicius <viniciusflexa@gmail.com>
2022-08-23 19:28:36 -0300

    pré renderização do conteúdo estático

--- aulao_nextjs/fundamentos/src/pages/index.jsx
+++ aulao_nextjs/fundamentos/src/pages/index.jsx
@@ -16,8 +16,9 @@ export default function Inicio(){
             <Navegador texto="JSX" destino="/jsx" cor="crimson"/>
             <Navegador texto="Navegação #01" destino="/navegacao" cor="green"/>
             <Navegador texto="Navegação #02" destino="/cliente/123" cor="blue"/>
-            <Navegador texto="Componente com estado" destino="/estado" cor="pink"/>
-            <Navegador texto="Integração com API #01" destino="/integracao_1" cor="yellow"/>
+            <Navegador texto="Componente com estado" destino="/estado" cor="#a45b71"/>
+            <Navegador texto="Integração com API #01" destino="/integracao_1" cor="#42a9a9"/>
+            <Navegador texto="Conteúdo Estático" destino="/estatico" cor="#fa054a"/>
         </div>
     )
 }
