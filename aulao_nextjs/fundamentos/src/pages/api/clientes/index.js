d3e03a0bdf3b4179cc14b467f5b426a5c54e5114
Vinicius <viniciusflexa@gmail.com>
2022-08-23 10:29:33 -0300

    usando API #1

---
+++ aulao_nextjs/fundamentos/src/pages/api/clientes/index.js
@@ -0,0 +1,16 @@
+export default function handler(req, res){
+    if(req.method === "GET"){
+        handleGet(req, res)
+    }else{
+        res.status(405).send()
+    }
+
+}
+
+function handleGet(req, res){
+    res.status(200).json({
+        id: 3,
+        nome: 'Maria',
+        idade: 33
+    })
+}
