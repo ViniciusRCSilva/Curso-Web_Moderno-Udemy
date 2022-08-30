f6eee6d4ae720c8287ef9b649833e7c63bb4275f
Vinicius <viniciusflexa@gmail.com>
2022-08-30 19:12:22 -0300

    criação da tabela de clientes

--- next-crud/src/pages/index.tsx
+++ next-crud/src/pages/index.tsx
@@ -1,7 +1,37 @@
+import Layout from "../components/Layout";
+import Tabela from "../components/Tabela";
+import Cliente from "../core/Cliente";
+
 export default function Home() {
+
+  const clientes = [
+    new Cliente('Ana', 34, '1'),
+    new Cliente('Bia', 45, '2'),
+    new Cliente('Carlos', 23, '3'),
+    new Cliente('Pedro', 54, '4')
+  ]
+
+  function clienteSelecionado(cliente: Cliente){
+
+  }
+
+  function clienteExcluido(cliente: Cliente){
+
+  }
+
   return (
-    <div className="">
-      
+    <div className={`
+      flex justify-center items-center h-screen
+      bg-gradient-to-r from-blue-500 to-purple-500
+      text-white
+    `}>
+      <Layout titulo="Cadastro Simples">
+        <Tabela 
+          clientes={clientes} 
+          clienteSelecionado={clienteSelecionado}
+          clienteExcluido={clienteExcluido}
+        ></Tabela>
+      </Layout>
     </div>
   )
 }
