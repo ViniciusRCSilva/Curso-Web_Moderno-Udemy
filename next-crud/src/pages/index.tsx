f9f4df193c96bfea93f852cc7af2426f34adf52d
Vinicius <viniciusflexa@gmail.com>
2022-09-28 19:40:10 -0300

    criando formulário

--- next-crud/src/pages/index.tsx
+++ next-crud/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Botao from "../components/Botao";
+import Formulario from "../components/Formulario";
 import Layout from "../components/Layout";
 import Tabela from "../components/Tabela";
 import Cliente from "../core/Cliente";
@@ -30,11 +31,14 @@ export default function Home() {
         <div className="flex justify-end">
           <Botao cor="green" className="mb-4">Novo Cliente</Botao>
         </div>
+
         <Tabela 
           clientes={clientes} 
           clienteSelecionado={clienteSelecionado}
           clienteExcluido={clienteExcluido}
         ></Tabela>
+
+        <Formulario cliente={clientes[0]}></Formulario>
       </Layout>
     </div>
   )
