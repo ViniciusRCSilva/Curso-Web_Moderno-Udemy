8c6fc5940fe5b7a88511d5e83a7249ccae8c3e76
Vinicius <viniciusflexa@gmail.com>
2022-09-29 14:53:02 -0300

    instalando e configurando o firebase

---
+++ next-crud/src/backend/config.ts
@@ -0,0 +1,12 @@
+import firebase from "../../node_modules/firebase/index";
+import 'firebase/firestore'
+
+if(!firebase.apps.length){
+    firebase.initializeApp({
+        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
+    })
+}
+
+export default firebase
