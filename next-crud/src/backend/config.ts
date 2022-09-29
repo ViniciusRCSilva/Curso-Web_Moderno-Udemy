5f497060ef2b0b6e463072985b18eaa38a399e4c
Vinicius <viniciusflexa@gmail.com>
2022-09-29 15:46:13 -0300

    integrando firebase ao cadastro

--- next-crud/src/backend/config.ts
+++ next-crud/src/backend/config.ts
@@ -1,11 +1,11 @@
-import firebase from "../../node_modules/firebase/index";
+import firebase from 'firebase'
 import 'firebase/firestore'
 
-if(!firebase.apps.length){
+if (!firebase.apps.length) {
     firebase.initializeApp({
         apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
         authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     })
 }
 
--- next-crud/src/pages/index.tsx
+++ next-crud/src/pages/index.tsx
@@ -1,28 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import ColecaoCliente from "../backend/db/ColecaoCliente";
 import Botao from "../components/Botao";
 import Formulario from "../components/Formulario";
 import Layout from "../components/Layout";
 import Tabela from "../components/Tabela";
 import Cliente from "../core/Cliente";
+import ClienteRepositorio from "../core/ClienteRepositorio";
 
 export default function Home() {
+
+  const repo: ClienteRepositorio = new ColecaoCliente()
+
   const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela')
   const [cliente, setCliente] = useState<Cliente>(Cliente.vazio())
+  const [clientes, setClientes] = useState<Cliente[]>([])
 
-  const clientes = [
-    new Cliente('Ana', 34, '1'),
-    new Cliente('Bia', 45, '2'),
-    new Cliente('Carlos', 23, '3'),
-    new Cliente('Pedro', 54, '4')
-  ]
+  useEffect(obterTodos, [])
+  
+  function obterTodos(){
+    repo.obterTodos().then(clientes => {
+        setClientes(clientes)
+        setVisivel('tabela')
+    })
+  }
 
   function clienteSelecionado(cliente: Cliente){
     setCliente(cliente)
     setVisivel('form')
   }
 
-  function clienteExcluido(cliente: Cliente){
-
+  async function clienteExcluido(cliente: Cliente){
+    await repo.excluir(cliente)
+    obterTodos()    
   }
 
   function novoCliente(){
@@ -30,8 +39,9 @@ export default function Home() {
     setVisivel('form')
   }
 
-  function salvarCliente(cliente: Cliente){
-    setVisivel('tabela')
+  async function salvarCliente(cliente: Cliente){
+    await repo.salvar(cliente)
+    obterTodos()
   }
 
   return (
